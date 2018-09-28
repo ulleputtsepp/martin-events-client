22e12b353381a3340b609051d40190e218435854
VSo17 <ulle.puttsepp@khk.ee>
2018-09-28 10:26:05 +0300

    Create new event where feature:true, added in index controller and index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+      @events = Event.where(featured:true)
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_27_102533) do
+ActiveRecord::Schema.define(version: 2018_09_28_060135) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2018_09_27_102533) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured"
   end
 
 end
