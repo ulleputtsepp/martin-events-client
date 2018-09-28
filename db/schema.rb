1cdee4bb9849b64799a64b3b9170cae9d55e2c39
VSo17 <ulle.puttsepp@khk.ee>
2018-09-28 13:30:49 +0300

    Rails association between events and event_categories created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_074050) do
+ActiveRecord::Schema.define(version: 2018_09_28_091755) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_074050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "featured"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,8 +45,13 @@ ActiveRecord::Schema.define(version: 2018_09_28_074050) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
