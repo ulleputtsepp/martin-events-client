5f47c6fc22993a670665a90f96a26f02fe35c1bb
VSo17 <ulle.puttsepp@khk.ee>
2018-09-28 15:53:12 +0300

    generated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Adding multiple categories
+Category.create([{name: 'Muusika'}, {name: 'Teater' }])
+# Adding one event
+Event.create({
+	name: "Lorem ipsum",
+	short_description: "Lorem ipsum doloer sit amet, ...",
+	long_description: "Lorem ipsum doloer sit amet, consectetur ...",
+	price: 3000,
+	date: Date.today + 3.weeks,
+	location: "Tartu",
+	category: Category.where(name: "Muusika").first
+})
+
+Event.create({
+	name: "Lorem ipsum",
+	short_description: "Lorem ipsum doloer sit amet, ...",
+	long_description: "Lorem ipsum doloer sit amet, consectetur ...",
+	price: 3000,
+	date: Date.today + 3.days,
+	location: "Tartu",
+	category: Category.where(name: "Muusika").first
+})
+
+Event.create({
+	name: "Lorem ipsum",
+	short_description: "Lorem ipsum doloer sit amet, ...",
+	long_description: "Lorem ipsum doloer sit amet, consectetur ...",
+	price: 3000,
+	date: Date.today + 3.years,
+	location: "Tartu",
+	category: Category.where(name: "Muusika").first
+})
+
+Event.create({
+	name: "Lorem ipsum",
+	short_description: "Lorem ipsum doloer sit amet, ...",
+	long_description: "Lorem ipsum doloer sit amet, consectetur ...",
+	price: 3000,
+	date: Date.today - 3. years + 2.days,
+	location: "Tartu",
+	category: Category.where(name: "Muusika").first
+})
