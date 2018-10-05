1d0638ed518657a5a15da8aa7bef1ab901ff7a81
VSo17 <ulle.puttsepp@khk.ee>
2018-10-05 16:00:11 +0300

    email sent

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'bcrypt', '~> 3.1.7'
 
 gem 'devise'
+gem 'sendgrid-ruby'
+gem 'sidekiq'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
---
+++ app/models/send_mail.rb
@@ -0,0 +1,13 @@
+class SendMail
+	def send
+		from = SendGrid::Email.new(email: 'test@example.com')
+to = SendGrid::Email.new(email: 'ulle.puttsepp@khk.ee')
+subject = 'Sending with SendGrid is Fun'
+content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+mail = SendGrid::Mail.new(from, subject, to, content)
+
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+response = sg.client.mail._('send').post(request_body: mail.to_json)
+
+	end
+end
