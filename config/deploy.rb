796b6da0c10e2cc2685f83be2266989c709bbdb9
VSo17 <ulle.puttsepp@khk.ee>
2018-10-18 13:48:45 +0300

    production.sqlite3 linked in db - deploy.br

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/srv/apps/ylle"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/master.key"
+append :linked_files, "config/master.key", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
